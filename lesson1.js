7a426596ead67e4d2844cc9c9f1e13c7079352d4
Sarojsur <rathindra101@gmail.com>
2025-03-20 21:59:26 +0600

    Add files via upload

---
+++ lesson1.js
@@ -0,0 +1,64 @@
+console.log("Welcome to java script")
+console.log("I am Saroj Sur")
+console.log(1)
+console.log("I read in class "+6)
+console.log("I country name is "+"Bangladesh")
+console.log("Cox's bazar is the longest sea beach in the "+"World")
+console.log("There are 195countries in the"+"world")
+console.log("Russia is biggest country in the" +"world")
+console.log("My roll number is"+2)
+console.log("Pele is the king of"+"football")
+console.log("Who"+"are"+"you")
+console.log("i can"+"swim")
+var people="Welcome to my J.S."
+console.log (people)
+var yard="The full form of J.S. is java script"
+console.log (yard)
+var pen="What is Pen"
+console.log (pen)
+var ball="what is ball"
+console.log (ball)
+var swiming="what is swiming"
+console.log (swiming)
+var king="who is the last king of Bengal"
+console.log (king)
+var clas="what is class 1"
+console.log (clas)
+var a=5 
+var b=3
+console.log (a+b)
+var q=5
+var v=100000
+console.log (q*v)
+var w=6500
+var j=191056555
+console.log(w/j)
+var g=597
+var n=678
+console.log(g-n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
