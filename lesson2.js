43bf5118b6fbad701de7366c29d075d1d0d7b491
Sarojsur <rathindra101@gmail.com>
2025-03-21 22:54:26 +0600

    Add files via upload

---
+++ lesson2.js
@@ -0,0 +1,27 @@
+function GM(){
+    console.log("Good Morning")
+}
+
+function GE(){
+    console.log("Good Evening")
+}
+
+function GN(){
+    console.log("Good Night")
+}
+
+function GM(){
+    console.log("Grand Master")
+}
+
+function GN(){
+    console.log("Good Morning")
+}
+
+function GN(){
+    console.log("Good Morning")
+}
+
+function GN(){
+    console.log("Good Morning")
+}
